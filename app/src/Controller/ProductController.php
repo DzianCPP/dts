d5a4b84531125b55517fd8b47c92f56441d1a540
DzianCPP <ocherkiprozaika@gmail.com>
2023-01-09 11:05:57 +0300

    Started product page, continue in a separate branch

---
+++ app/src/Controller/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product/{product_id}', name: 'app_product')]
+    public function index(): Response
+    {
+        return $this->render('user/product/main.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
