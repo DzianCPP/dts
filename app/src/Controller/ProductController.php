6e2ef820ebab17e13303b8de9ee5793623c0b118
Denis Martynovich <denis.martynovich@innowise-group.com>
2023-01-09 14:36:36 +0000

    Merged in product_page_services_block (pull request #16)
    added services
    
    * added services

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -11,8 +11,18 @@ class ProductController extends AbstractController
     #[Route('/product/{product_id}', name: 'app_product')]
     public function index(): Response
     {
-        return $this->render('user/product/main.html.twig', [
+        $data = [
             'controller_name' => 'ProductController',
-        ]);
+            'services' => [
+                [ 'name' => 'Установка ПО и ОС', 'price' => '100'],
+                [ 'name' => 'Расширенная гарантия', 'price' => '120'],
+                [ 'name' => 'Обклейка защитной пленкой', 'price' => '150'],
+                [ 'name' => 'Гравировка клавиатуры', 'price' => '100'],
+                [ 'name' => 'Установка пленки на экран', 'price' => '50'],
+                [ 'name' => 'Нанесение рисунка на корпус', 'price' => '25']                
+            ]
+        ];
+        
+        return $this->render('user/product/main.html.twig', $data);
     }
 }
